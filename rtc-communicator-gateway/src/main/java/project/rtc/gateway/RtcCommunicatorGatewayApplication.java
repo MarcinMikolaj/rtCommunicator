4dccf671de72e35fe9990e89f098c5d7f4e187f0
Marcin Mikołajczyk <marcin.mikolajczyk22@gmail.com>
2023-04-08 00:32:40 +0200

    #35 Added gateway service

---
+++ rtc-communicator-gateway/src/main/java/project/rtc/gateway/RtcCommunicatorGatewayApplication.java
@@ -0,0 +1,11 @@
+package project.rtc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcCommunicatorGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RtcCommunicatorGatewayApplication.class, args);
+	}
+}
---
+++ rtc-communicator-gateway/src/test/java/project/rtc/gateway/RtcCommunicatorGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package project.rtc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcCommunicatorGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
